caaff827d4f4314e49979f0dc155f262c3e0e5bf
szoftverfejleszto2016 <szoftverfejleszto2016@gmail.com>
2018-01-27 10:46:06 +0100

    Új főprogram.

--- src/konyvek/Konyvek.java
+++ src/konyvek/Konyvek.java
@@ -1,5 +1,7 @@
 package konyvek;
 
+import java.util.Scanner;
+
 /**
  *
  * @author tothj
@@ -8,7 +10,34 @@ public class Konyvek {
 
     public static void main(String[] args) {
         DB ab = new DB();
-        ab.kiir(1972);
+        Scanner bill = new Scanner(System.in, "cp1250");
+        boolean tovabb = true;
+        String szerzo, cim, eredeti;
+        int ev;
+        
+        while (tovabb) {
+            System.out.println("1-lista 2-új 3-törlés 0-kilépés");
+            System.out.print("válasz: ");
+            String v = bill.nextLine();
+            switch (v.charAt(0)) {
+                case '1':
+                    System.out.print("Év: ");
+                    ev = bill.nextInt();
+                    bill.nextLine();
+                    ab.kiir(ev);
+                    break;
+                case '2':
+                    
+                    break;
+                case '3':
+                    
+                    break;
+                default:
+                    tovabb = false;
+                    break;
+            }
+        }
+        
     }
     
 }
